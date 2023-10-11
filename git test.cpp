da4c338b3a3e08e3ed0b0ba592461a856a10f9cc
itou-syunnya-0826 <k023g1015@g.neec.ac.jp>
2023-10-11 10:10:40 +0900

    名前を追加

--- git test.cpp
+++ git test.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello Git Hub!\n";
 
-    std::cout << "Hello Git Hub!\n";
+    std::cout << "GC1D_02_イトウシュンヤ\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
